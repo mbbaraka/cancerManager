b5ee2efc32a66f5f54541b14a5527d5209529f1e
Baraka Mark Bright <markbrightbaraka@gmail.com>
2021-10-19 08:41:57 -0700

    Working on authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,14 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- vendor css -->
+    <link href="{{ asset('cancerManager/lib/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('cancerManager/lib/ionicons/css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('cancerManager/lib/typicons.font/typicons.css') }}" rel="stylesheet">
+    <link href="{{ asset('cancerManager/lib/flag-icon-css/css/flag-icon.min.css') }}" rel="stylesheet">
 
+    <!-- azia CSS -->
+    <link rel="stylesheet" href="{{ asset('cancerManager/css/azia.css') }}">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -76,5 +83,17 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('cancerManager/lib/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('cancerManager/lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('cancerManager/lib/ionicons/ionicons.js') }}"></script>
+    <script src="{{ asset('cancerManager/lib/jquery.flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('cancerManager/lib/jquery.flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('cancerManager/lib/chart.js/Chart.bundle.min.js') }}"></script>
+    <script src="{{ asset('cancerManager/lib/peity/jquery.peity.min.js') }}"></script>
+
+    <script src="{{ asset('cancerManager/js/azia.js') }}"></script>
+    <script src="{{ asset('cancerManager/js/chart.flot.sampledata.js') }}"></script>
+    <script src="{{ asset('cancerManager/js/dashboard.sampledata.js') }}"></script>
+    <script src="{{ asset('cancerManager/js/jquery.cookie.js') }}" type="text/javascript"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// // Route::get('/', '[HomeController::index]')->name('home');
+// Route::get('/', function () {
+
+// });
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+
+
+});
