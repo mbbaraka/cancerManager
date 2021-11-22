aa24f72df2a6533f827af9a14955145cea5a9666
mbbaraka <markbrightbaraka@gmail.com>
2021-11-22 08:45:05 -0800

    CHanges to the dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Diagnosis;
 use App\Models\Patient;
+use Faker\Calculator\Luhn;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -28,18 +29,38 @@ class HomeController extends Controller
         /** Managing dashboard */
 
         // location
-        $location_pajulu = Patient::get()->where('sub_county', 'Pajulu')->count();
-        $location_oluko = Patient::get()->where('sub_county', 'Oluko')->count();
-        $location_adumi = Patient::get()->where('sub_county', 'Adumi')->count();
-        $location_logiri = Patient::get()->where('sub_county', 'Logiri')->count();
         $location = Patient::get()->count();
+        if ($location <= 0) {
+            $location = 1;
+            $location_pajulu = 1;
+            $location_adumi = 1;
+            $location_logiri = 1;
+            $location_oluko = 1;
+
+        }else{
+            $location_pajulu = Patient::get()->where('sub_county', 'Pajulu')->count();
+            $location_oluko = Patient::get()->where('sub_county', 'Oluko')->count();
+            $location_adumi = Patient::get()->where('sub_county', 'Adumi')->count();
+            $location_logiri = Patient::get()->where('sub_county', 'Logiri')->count();
+        }
+
 
         // cancer types
         $cancer = Diagnosis::get()->count();
-        $lung_cancer = Diagnosis::get()->where('type', 'Lung Cancer')->count();
-        $skin_cancer = Diagnosis::get()->where('type', 'Skin Cancer')->count();
-        $breast_cancer = Diagnosis::get()->where('type', 'Breast Cancer')->count();
-        $cervical_cancer = Diagnosis::get()->where('type', 'Cervical Cancer')->count();
+
+        if ($cancer <= 0) {
+            $cancer = 1;
+            $lung_cancer = 1;
+            $breast_cancer = 1;
+            $skin_cancer = 1;
+            $cervical_cancer = 1;
+        }else{
+            $lung_cancer = Diagnosis::get()->where('type', 'Lung Cancer')->count();
+            $skin_cancer = Diagnosis::get()->where('type', 'Skin Cancer')->count();
+            $breast_cancer = Diagnosis::get()->where('type', 'Breast Cancer')->count();
+            $cervical_cancer = Diagnosis::get()->where('type', 'Cervical Cancer')->count();
+        }
+
 
         return view('home.index',
         compact(
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -10,14 +10,14 @@
           <a href="" class="close">&times;</a>
         </div><!-- az-header-menu-header -->
         <ul class="nav">
-          <li class="nav-item">
-            <a href="{{ url('/') }}" class="nav-link"><i class="typcn typcn-chart-area-outline"></i> Dashboard</a>
+          <li class="nav-item active show">
+            <a href="index.html" class="nav-link"><i class="typcn typcn-chart-area-outline"></i> Dashboard</a>
           </li>
           <li class="nav-item">
             <a href="" class="nav-link with-sub"><i class="typcn typcn-document"></i> Patients</a>
             <nav class="az-menu-sub">
               <a href="{{ route('new-patient') }}" class="nav-link">Add New</a>
-              <a href="{{ route('patient-index') }}" class="nav-link">View All</a>
+              <a href="{{ route('referred-patients') }}" class="nav-link">View All</a>
             </nav>
           </li>
           <li class="nav-item">
@@ -46,7 +46,7 @@
                 </div><!-- media-body -->
               </div><!-- media -->
               <div class="media">
-                <div class="az-img-user"><img src="../img/faces/face5.jpg" alt=""></div>
+                <div class="az-img-user"><img src="{{ asset('cancerManager/img/faces/face5.jpg') }}" alt=""></div>
                 <div class="media-body">
                   <p><strong>Adrian Monino</strong> submitted a new case</p>
                   <span>Mar 12 10:40pm</span>
@@ -57,7 +57,7 @@
           </div><!-- dropdown-menu -->
         </div><!-- az-header-notification -->
         <div class="dropdown az-profile-menu">
-          <a href="" class="az-img-user"><img src="../img/faces/face1.jpg" alt=""></a>
+          <a href="" class="az-img-user"><img src="{{ asset('cancerManager/img/faces/face1.jpg') }}" alt=""></a>
           <div class="dropdown-menu">
             <div class="az-dropdown-header d-sm-none">
               <a href="" class="az-header-arrow"><i class="icon ion-md-arrow-back"></i></a>
